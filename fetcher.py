d7ae6ebe5d5bbbed7c7c3bb92ce6e9d5d1df506e
Raphaël Vinot <raphael@vinot.info>
2023-08-21 17:53:41 +0200

    new: Make graphs

--- fetcher.py
+++ fetcher.py
@@ -7,6 +7,11 @@ from pathlib import Path
 
 import requests
 
+#import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.dates import AutoDateLocator, AutoDateFormatter, HOURLY
+#import numpy as np
+
 
 class BordeauxPoolUse():
 
@@ -46,3 +51,27 @@ class BordeauxPoolUse():
             day_content[update_timestamp.isoformat()] = fields
             with day_file.open('w') as _fw:
                 json.dump(day_content, _fw, indent=2)
+
+    def make_graphs(self):
+        monthly_dirs = {filename.parent for filename in self.root_storage.rglob('*.json')}
+        for monthly_dir in monthly_dirs:
+            occupancy = []
+            record_time = []
+            for jsonfile in sorted(list(monthly_dir.iterdir())):
+                with jsonfile.open() as f:
+                    day_content = json.load(f)
+                for entry in day_content.values():
+                    if entry['fmicourante'] > 0:
+                        occupancy.append(entry['fmicourante'])
+                        record_time.append(datetime.fromisoformat(entry['datemiseajour']).astimezone())
+            if occupancy and record_time:
+                fig, ax = plt.subplots()
+                plt.title(str(monthly_dir))
+                plt.plot(record_time, occupancy)
+                locator = AutoDateLocator()
+                locator.intervald[HOURLY] = [3]
+                ax.xaxis.set_major_locator(locator)
+                ax.xaxis.set_major_formatter(AutoDateFormatter(locator))
+                plt.xticks(rotation=45)
+                plt.savefig(monthly_dir / 'graph.png')
+                # plt.show()
