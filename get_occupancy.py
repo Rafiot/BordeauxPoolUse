f07a306ec75cc4db354fcf2c18cf7f13936ace72
server <autoupdate@local>
2026-09-09 23:12:52 +0000

    auto push

--- get_occupancy.py
+++ get_occupancy.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
+
 from fetcher import BordeauxPoolUse
 
-bpu = BordeauxPoolUse()
+from git import Repo
 
-bpu.make_jsons()
 
-# bpu.make_graphs()
+def update_repo() -> None:
+    repo = Repo('./')
+    to_add = ['data']
+    repo.index.add(to_add)
+    repo.index.commit(f"Add data up to {datetime.now()}")
+    origin = repo.remote(name='origin')
+    origin.push()
+
+
+if __name__ == "__main__":
+    bpu = BordeauxPoolUse()
+    bpu.make_jsons()
+    # bpu.make_graphs()
+    
+    update_repo()
